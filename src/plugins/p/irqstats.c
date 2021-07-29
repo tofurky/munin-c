b86f839f64f80bc25830b8a4a6610c7c57cc3fde
Matt Merhar <mattmerhar@protonmail.com>
2021-07-28 22:45:27 -0400

    rename irqstats[] to avoid shadow warning from Cppcheck
    rename a couple array indexes

--- src/plugins/p/irqstats.c
+++ src/plugins/p/irqstats.c
@@ -72,7 +72,7 @@ bool endswith(char *haystack, char *needle)
 	return !strcmp((char *)(haystack + haystack_length - needle_length), needle);
 }
 
-size_t read_interrupts(irqstat_t irqstats[], bool config)
+size_t read_interrupts(irqstat_t irqs[], bool config)
 {
 	FILE *interrupts;
 	char line_buf[MAX_LINE] = {0};
@@ -141,22 +141,22 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 		}
 
 		length = strlen(pos);
-		irqstats[irq_num].name = malloc(length); /* We're discarding the ':' */
-		if (!irqstats[irq_num].name) {
+		irqs[irq_num].name = malloc(length); /* We're discarding the ':' */
+		if (!irqs[irq_num].name) {
 			fprintf(stderr, "malloc: %m\n");
 			return 0;
 		}
 
-		strncpy(irqstats[irq_num].name, pos, --length); /* Discard ':' */
-		irqstats[irq_num].name[length] = '\0';
+		strncpy(irqs[irq_num].name, pos, --length); /* Discard ':' */
+		irqs[irq_num].name[length] = '\0';
 
 		/* Convert each counter value to long and add it to the total for this IRQ */
-		for (size_t cpu_idx = 0; cpu_idx <= cpu_num; cpu_idx++) {
+		for (size_t c = 0; c <= cpu_num; c++) {
 			pos = strtok(NULL, " ");
 			/* Some interrupts, such as 'ERR' or 'MIS' will only have a single counter rather than one per CPU */
 			if (!pos) {
-				if (!cpu_idx) {
-					fprintf(stderr, "irqstats[%zu].name = '%s' has no counters\n", irq_num, irqstats[irq_num].name);
+				if (!c) {
+					fprintf(stderr, "irqs[%zu].name = '%s' has no counters\n", irq_num, irqs[irq_num].name);
 					return 0;
 				}
 
@@ -165,15 +165,15 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 
 			/* If it's not a positive integer, we may have run into the description unexpectedly */
 			if (!isnumeric(pos)) {
-				if (!cpu_idx) {
-					fprintf(stderr, "irqstats[%zu].name = '%s' has only garbage '%s'\n", irq_num, irqstats[irq_num].name, pos);
+				if (!c) {
+					fprintf(stderr, "irqs[%zu].name = '%s' has only garbage '%s'\n", irq_num, irqs[irq_num].name, pos);
 					return 0;
 				}
 
 				break;
 			}
 
-			irqstats[irq_num].count += strtol(pos, NULL, 10);
+			irqs[irq_num].count += strtol(pos, NULL, 10);
 		}
 
 		/* Skip over the description parsing unless we're running 'config' */
@@ -191,16 +191,16 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 		for (char *eos = pos + strlen(pos) - 1; *eos && isspace(*eos); *eos-- = '\0');
 
 		/* It won't be any longer than this */
-		irqstats[irq_num].description = malloc(strlen(pos) + 1);
-		if (!irqstats[irq_num].description) {
+		irqs[irq_num].description = malloc(strlen(pos) + 1);
+		if (!irqs[irq_num].description) {
 			fprintf(stderr, "malloc: %m\n");
 			return 0;
 		}
-		*irqstats[irq_num].description = '\0';
+		*irqs[irq_num].description = '\0';
 
 		/* If it's not a numbered IRQ, then the description is simply everything that remains */
-		if (!isnumeric(irqstats[irq_num].name)) {
-			strcpy(irqstats[irq_num].description, pos);
+		if (!isnumeric(irqs[irq_num].name)) {
+			strcpy(irqs[irq_num].description, pos);
 			goto next_line;
 		}
 
@@ -211,7 +211,7 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 
 		/* If we only have one token, there's nothing left to parse */
 		if (!token_num) {
-			strcpy(irqstats[irq_num].description, pos);
+			strcpy(irqs[irq_num].description, pos);
 			goto next_line;
 		}
 
@@ -221,8 +221,8 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 			token_start = 2;
 			/* SPARC has been seen to have many duplicate 'MSIQ' interrupts (one per thread), so always show the IRQ number here to differentiate */
 			if (!strcmp(tokens[2], "MSIQ")) {
-				irqstats[irq_num].hwirq = strtoul(irqstats[irq_num].name, NULL, 10);
-				irqstats[irq_num].has_hwirq = true;
+				irqs[irq_num].hwirq = strtoul(irqs[irq_num].name, NULL, 10);
+				irqs[irq_num].has_hwirq = true;
 			}
 #else
 		/* Newer ARM, MIPS, some x86, etc. */
@@ -241,9 +241,9 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 			/* 33:     617373  f1010140.gpio  17 Edge      pps.-1 */
 			/* Interrupt 33, for device(s): pps.-1 [17] */
 
-			if ((hwirq = strtoul(tokens[1], NULL, 10)) != strtoul(irqstats[irq_num].name, NULL, 10)) {
-				irqstats[irq_num].hwirq = hwirq;
-				irqstats[irq_num].has_hwirq = true;
+			if ((hwirq = strtoul(tokens[1], NULL, 10)) != strtoul(irqs[irq_num].name, NULL, 10)) {
+				irqs[irq_num].hwirq = hwirq;
+				irqs[irq_num].has_hwirq = true;
 			}
 
 			/* MIPS has been seen to not show the type */
@@ -259,20 +259,20 @@ size_t read_interrupts(irqstat_t irqstats[], bool config)
 			if (isdigit(*tokens[1]) && (endswith(tokens[1], "-fasteoi") || endswith(tokens[1], "-edge"))) {
 				token_start = 2;
 
-				if ((hwirq = strtoul(tokens[1], NULL, 10)) != strtoul(irqstats[irq_num].name, NULL, 10)) {
-					irqstats[irq_num].hwirq = hwirq;
-					irqstats[irq_num].has_hwirq = true;
+				if ((hwirq = strtoul(tokens[1], NULL, 10)) != strtoul(irqs[irq_num].name, NULL, 10)) {
+					irqs[irq_num].hwirq = hwirq;
+					irqs[irq_num].has_hwirq = true;
 				}
 			}
 		}
 
 		/* Concatenate the needed tokens with a space */
-		for (*irqstats[irq_num].description = '\0'; token_start <= token_num; token_start++) {
-			strcat(irqstats[irq_num].description, tokens[token_start]);
-			strcat(irqstats[irq_num].description, " ");
+		for (*irqs[irq_num].description = '\0'; token_start <= token_num; token_start++) {
+			strcat(irqs[irq_num].description, tokens[token_start]);
+			strcat(irqs[irq_num].description, " ");
 		}
 
-		*(irqstats[irq_num].description + strlen(irqstats[irq_num].description) - 1) = '\0';
+		*(irqs[irq_num].description + strlen(irqs[irq_num].description) - 1) = '\0';
 
 next_line:
 			line_num++;
@@ -297,8 +297,8 @@ bool irqstats_autoconf()
 }
 
 bool irqstats_config() {
-	irqstat_t irqstats[MAX_IRQS] = {0};
-	size_t irq_num = read_interrupts(irqstats, true);
+	irqstat_t irqs[MAX_IRQS] = {0};
+	size_t irq_num = read_interrupts(irqs, true);
 
 	if (irq_num == 0) {
 		fprintf(stderr, "no interrupts found\n");
@@ -313,27 +313,27 @@ bool irqstats_config() {
 
 	printf("graph_order");
 	for (size_t i = 0; i < irq_num; i++)
-		printf(" i%s", irqstats[i].name);
+		printf(" i%s", irqs[i].name);
 	printf("\n");
 
 	for (size_t i = 0; i < irq_num; i++) {
 		/* Some, like ERR and MIS, do not have a description */
-		printf("i%s.label %s", irqstats[i].name, (irqstats[i].description ? irqstats[i].description : irqstats[i].name));
-		if (irqstats[i].has_hwirq)
-			printf(" [%lu]", irqstats[i].hwirq);
+		printf("i%s.label %s", irqs[i].name, (irqs[i].description ? irqs[i].description : irqs[i].name));
+		if (irqs[i].has_hwirq)
+			printf(" [%lu]", irqs[i].hwirq);
 		printf("\n");
 
-		if (irqstats[i].description) {
-			printf("i%s.info Interrupt %s, for device(s): %s", irqstats[i].name, irqstats[i].name, irqstats[i].description);
-			if (irqstats[i].has_hwirq)
-				printf(" [%lu]", irqstats[i].hwirq);
+		if (irqs[i].description) {
+			printf("i%s.info Interrupt %s, for device(s): %s", irqs[i].name, irqs[i].name, irqs[i].description);
+			if (irqs[i].has_hwirq)
+				printf(" [%lu]", irqs[i].hwirq);
 			printf("\n");
 		}
 		/* NOTE: The original Perl plugin does a case insensitive substring match. '#define _GNU_SOURCE' and strcasestr() could imitate this. */
-		else if (!strcmp(irqstats[i].name, "NMI")) {
+		else if (!strcmp(irqs[i].name, "NMI")) {
 			printf("iNMI.info Non-maskable interrupt. Either 0 or quite high. If it's normally 0 then just one NMI will often mark some hardware failure.\n");
 		}
-		else if (!strcmp(irqstats[i].name, "LOC")) {
+		else if (!strcmp(irqs[i].name, "LOC")) {
 			printf("iLOC.info Local (per CPU core) APIC timer interrupt. Until 2.6.21 normally 250 or 1000 per second. On modern 'tickless' kernels it more or less reflects how busy the machine is.\n");
 		}
 		else {
@@ -341,10 +341,10 @@ bool irqstats_config() {
 		}
 
 		printf("i%s.type DERIVE\n"
-			"i%s.min 0\n", irqstats[i].name, irqstats[i].name);
+			"i%s.min 0\n", irqs[i].name, irqs[i].name);
 
-		free(irqstats[i].name);
-		free(irqstats[i].description);
+		free(irqs[i].name);
+		free(irqs[i].description);
 	}
 
 	return true;
@@ -352,8 +352,8 @@ bool irqstats_config() {
 
 bool irqstats_fetch()
 {
-	irqstat_t irqstats[MAX_IRQS] = {0};
-	size_t irq_num = read_interrupts(irqstats, false);
+	irqstat_t irqs[MAX_IRQS] = {0};
+	size_t irq_num = read_interrupts(irqs, false);
 
 	if (irq_num == 0) {
 		fprintf(stderr, "no interrupts found\n");
@@ -361,10 +361,10 @@ bool irqstats_fetch()
 	}
 
 	for (size_t i = 0; i < irq_num; i++) {
-		printf("i%s.value %lu\n", irqstats[i].name, irqstats[i].count);
+		printf("i%s.value %lu\n", irqs[i].name, irqs[i].count);
 
-		free(irqstats[i].name);
-		free(irqstats[i].description);
+		free(irqs[i].name);
+		free(irqs[i].description);
 	}
 
 	return true;
